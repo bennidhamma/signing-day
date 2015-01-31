fa2326207267d9cd4a34827a1ddd607811913af3
Ben Joldersma <ben@scout.com>
2015-01-30 18:22:10 -0800

    first draft of player cards in

--- react-0.12.2/src/player-card.js
+++ react-0.12.2/src/player-card.js
@@ -1,26 +1,41 @@
 /* global React */
 
+var React = React || {};
+
 var PlayerCard = React.createClass({
   render: function() { 
     var p = this.props.player;
+    var y = p.playerYears[0];
+    var photoUrl = 'http://imgix.scout.com/' + ~~(y.primaryPhotoId / 10000) + '/' + y.primaryPhotoId + '.jpg?w=150&h=150&fit=crop&crop=faces';
     var stars = [];
     for (var i = 1; i <= 5; i++) {
-      stars.push(<div className={i <= p.rating ? 'fille-star' : 'open-star'}/>);
+      stars.push(<div className={i <= y.rating ? 'filled star' : 'open star'}>★</div>);
     }
-    return <div>
-      <img class="head-shot" src={p.player.photoUrl}/>
-      <div class="stars">{stars}</div>
-    </div>
+    var interests = ['Signed', 'Verbal', 'Soft Verbal', 'High', 'Medium', 'Low', 'None'];
+    var interestDivs = interests.map(function(i) {
+      return <div className={'interest ' + i.toLowerCase().replace(' ', '-')}>{i}</div>;
+    });
+    return <div className="player-card">
+      <img className="head-shot" src={photoUrl}/>
+      <div className="info">
+        <div className="position">{y.primaryPosition.abbreviation}</div>
+        <div className="stars">{stars}</div>
+        <h1>{p.name}</h1>
+      </div>
+      <div className="interests">{interestDivs}</div>
+    </div>;
   }
 });
 
-var player = {
-  photoUrl: 'http://imgix.scout.com/132/1325338.jpg?w=240&h=240&fit=crop&crop=faces',
-  rating: 4,
-  name: 'Kahlil McKenzie'
-};
+$.getJSON(
+  'http://api.scout.com//topic/players/search?from=0&size=27&team="USC"&sortBy=CommitDate&category=Football+Recruiting&classYear=2015&league=HS',
+  function(players) {
+    var playerCards = players.searchResults.results.map(function(p) {
+      return <PlayerCard player={p}/>;
+    });
 
-React.render(
-  <PlayerCard player={player}/>
-  document.getElementById('example')
-);
+    React.render(
+      <div>{playerCards}</div>,
+      document.getElementById('example')
+    );
+  });
